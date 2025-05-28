edb5d5792151f4f6106a84feb03a75e6b27d3f3c
soniaakter08 <asonia3308@gmail.com>
2025-05-28 14:45:58 +0300

    Fix json-server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-'use strict';
+// server.js
+import jsonServer from "json-server";
 
-module.exports = require('./server.node');
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// Disable static file serving
+const middlewares = jsonServer.defaults({
+  static: false,
+});
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`✅ JSON Server is running on port ${PORT}`);
+});
